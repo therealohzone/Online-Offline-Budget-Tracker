d8331797cd4bad6e759a0aa581153f64f7b23e2f
johnoh <johnoh@usc.edu>
2021-11-24 02:14:06 -0500

    add db.js and service-workers.js

---
+++ public/db.js
@@ -0,0 +1,14 @@
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({ target }) => {
+  let db = target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+var urlsToCache = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
